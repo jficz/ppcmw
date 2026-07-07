e0b2ff1ccee0e555389b843f82fbb60c60b4aaf2
Jakub Fišer <j@jfi.cz>
2026-07-07 18:31:44 +0200

    feat(cache): Introduce aggressive caching
    to improve performance. All cache is handled
    asynchronously to not block the menu and increase
    responsiveness significantly at the expense of
    possibly not being fully up-to-date.
    
    The one exception is when the cache is empty, then
    the menu only shows after the cache is populated.
    
    Other minor fixes and updates
     - don't output anything if ESC is pressed
       (i.e. when `fuzzel` exits with non-zero status)
     - do not require PAT. User can still provide it
       to automate login but if no PAT is provided,
       the session creation is deferred to the user
       instead of hard fail

--- README.md
+++ README.md
@@ -11,6 +11,13 @@ associated with the selected one, akin to [`passmenu`](https://git.zx2c4.com/pas
 
 Only considers items of type `login` and state `active`.
 
+Utilizes aggressive caching to improve performance and responsiveness
+as `pass-cli` is very much _not_ optimized for this kind of use case.
+
+Without cache, each call would take up to 3s to display the menu.
+
+See Security#Cache for details.
+
 ## Dependencies
 
 All requirements are hardcoded:
@@ -28,7 +35,7 @@ if XDG_CONFIG_HOME is not defined.
 ### Config params
 
 `vault_name`: name of the vault items should be pulled from
-`PAT`: personal access token
+`PAT`: personal access token, optional
 
 ### How to obtain PAT and enable access to your vault
 
@@ -45,6 +52,11 @@ pass-cli pat access grant --vault-name <vault_name> --role viewer --personal-acc
 No security checks and audits have been done (yet). Storing
 your PAT in the conf file as plaintext is bad.
 
+You don't _need_ to store PAT in your config but if you don't,
+you must make sure a valid `pass-cli` session exists before
+you invoke the script. If you provide the PAT, the scrip
+will log in with that PAT for you if needed.
+
 You can store the PAT in another password manager, like `pass`
 for example and put this in your config:
 ```
@@ -53,13 +65,33 @@ PAT=$(pass my_pass-cli_token)
 
 That's _slightly_ better.
 
+### Cache
+
+For performance reasons the script utilizes item list cache (item title
+and item id pairs). This cache is plaintext and stored, together with
+Vault share_id, in `$XDG_STATE_HOME/ppcmw/*`.
+
+
 ## Future plans
 
-- optimize performance
-- possibly introduce some caching
+### TODO
+
 - support for other `dmenu`-like implementations
 - support for other `wtype`-like implementations
 - support for non-wayland environments
 - support for more secret types and fields
 - support for search over more metadata, not just the title
 - security review
+
+### Implemented
+
+- ~optimize performance~ [✔]
+- ~possibly introduce some caching~ [✔]
+
+Cache for items list implemented, stored in plaintext. Rebuilds cache
+in background with each invocation but doesn't wait for it to build
+to display the menu _unless_ the cache is empty (i.e. first start).
+
+A few minor performance tweaks introduced:
+  - use cached `--share-id` for Vault identification - slightly faster than vault name
+  - a couple of async calls, related to cache population
--- ppcmw.sh
+++ ppcmw.sh
@@ -1,33 +1,81 @@
 #!/usr/bin/env bash
 
-_conf="${XDG_CONFIG_HOME:-~/.config}/ppcmw.conf"
-
-[[ -f "$_conf" ]] && source "$_conf"
-
-_vault=${vault_name:-Personal}
-_pcli=${pass_cli_bin:-"pass-cli"}
+# basic functions
+function log() {
+  echo "$*" > /dev/stderr
+}
 
 function die() {
-  echo "$*" > /dev/stderr
+  log "$*"
   exit 1
 }
 
-function call-pass() {
-  if [[ "$*" =~ "item list " ]]; then
-    vopt=("$_vault")
-  else
-    vopt=("--vault-name" "$_vault")
-  fi
-  pass-cli "$@" --output json ${vopt[@]}
-}
+# config
+_conf="${XDG_CONFIG_HOME:-"$HOME/.config"}/ppcmw.conf"
+_state="${XDG_STATE_HOME:-"$HOME/.local/state"}/ppcmw"
 
+[[ -f "$_conf" ]] && source "$_conf"
+
+# fail fast if cli not found
+_pcli=${pass_cli_bin:-"pass-cli"}
 [[ -x $(command -v $_pcli) ]] || die "Pass-cli binary not found: $_pcli"
 
-if [[ -z "${PAT}" ]]; then
-  die "PAT not set!"
+
+# create or include state
+mkdir -p "$_state"
+
+declare -A state
+for st in vault_share_id items_cache; do
+  state[$st]=$([[ -f "$_state/$st" ]] && cat "${_state}/${st}")
+done
+
+# defaults
+_vault=${vault_name:-Personal}
+
+
+# runtime functions
+
+function call-pass() {
+  pass-cli "$@" --output json --share-id "${state[vault_share_id]}"
+}
+
+if ! $_pcli test; then
+  if [[ -n "$PAT" ]]; then
+    PROTON_PASS_PERSONAL_ACCESS_TOKEN="$PAT" \
+    $_pcli login \
+    || die "Cannot log in with provided PAT"
+  else
+    die "No active session and PAT not provided!"
+  fi
 fi
 
-$_pcli test || PROTON_PASS_PERSONAL_ACCESS_TOKEN="$PAT" $_pcli login
+# cache vault share id if needed, in the background
+(
+  _id=$($_pcli vault list --output json | jq -r '.vaults[] | select(.name == "'"$_vault"'") | .share_id')
+  if [[ "$_id" != "${state[vault_share_id]}" ]]; then
+    echo "$_id" > "$_state"/vault_share_id
+  fi
+) &
+
+if [[ -z "${state[vault_share_id]}" ]]; then
+  wait
+  state[vault_share_id]=$(cat "$_state"/vault_share_id)
+fi
+
+
+# refresh items cache in the background
+(
+  _items_cache=$(call-pass item list --filter-type login --filter-state active | jq -r '.items[] | (.id,.title)')
+  if [[ ${#_items_cache} -ne ${#state[items_cache]} ]]; then
+    echo "$_items_cache" > "$_state"/items_cache
+  fi
+) &
+
+if [[ -z "${state[items_cache]}" ]]; then
+  wait
+  state[items_cache]=$(cat "$_state"/items_cache)
+fi
+
 
 declare -a _items_title _items_id
 
@@ -35,8 +83,10 @@ while read id; do
   read title
   _items_id+=("$id")
   _items_title+=("$title")
-done <<< $(call-pass item list --filter-type login --filter-state active | jq -r '.items[] | (.id,.title)')
+done <<< "${state[items_cache]}"
 
-ret_item_id=$(fuzzel --dmenu --index <<< $(printf '%s\n' "${_items_title[@]}"))
+if ret_item_id=$(fuzzel --dmenu --index <<< $(printf '%s\n' "${_items_title[@]}")); then
+  wtype $(call-pass item view --item-id "${_items_id[$ret_item_id]}" | jq -r '.item.content.content.Login.password')
+fi
 
-wtype $(call-pass item view --item-id "${_items_id[$ret_item_id]}" | jq -r '.item.content.content.Login.password')
+wait
